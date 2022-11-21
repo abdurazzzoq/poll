e9ee48cf2b588028ad580d2b0d2bf3546dd1e501
abdurazzzoq <salemialekum34@gmail.com>
2022-11-21 23:45:17 +0500

    jsgome

---
+++ app.js
@@ -0,0 +1,14 @@
+let name = prompt('What is your name?')
+let age = prompt(`${name} how old are you? `)
+if (age >= 18){
+    alert("you can recieve a driving license")
+}else{
+    alert("you are not allowed to get driving license")
+    alert(`${18-age} more years left to get driving license`)
+}
+
+
+
+
+
+
---
+++ app2.js
@@ -0,0 +1,29 @@
+let number = +prompt('enter number')
+let number2 = +prompt('enter number')
+let number3 = +prompt('enter number')
+
+
+
+if(number === number2 && number === number3 && number2 === number3){
+    alert('all numbers are equal')}
+else if(number >= number2 && number >= number3){
+    alert(`${number} is the biggest`)    
+}
+else if(number2 >= number && number2 >= number3){
+    alert(`${number2} is the biggest`)
+}
+else if(number3 >= number && number3 >= number2){
+    
+    alert(`${number3} is the biggest`)}
+
+  
+    
+    
+
+
+
+
+
+
+
+
